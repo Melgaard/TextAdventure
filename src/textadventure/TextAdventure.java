d49a140e1cd5eba5bff1f9012bfad1b0f5451a57
Melgaard <mmjworkgg@gmail.com>
2014-11-18 13:26:22 +0100

    first changes
    first changes

---
+++ src/textadventure/TextAdventure.java
@@ -0,0 +1,15 @@
+package textadventure;
+
+/**
+ *
+ * @author Blotto
+ */
+public class TextAdventure {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hey");
+        
+    }
+    
+}
