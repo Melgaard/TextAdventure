0e4932764ea75539807ab606c67743662fa5d295
ElinorMohr <ElinorMohr@users.noreply.github.com>
2014-11-25 12:11:34 +0100

    Update Light.java
    med nedarv

--- src/textadventure/Light.java
+++ src/textadventure/Light.java
@@ -1,9 +1,16 @@
 package textadventure;
 
-/**
- *
- * @author Blotto
- */
-public class Light {
+public class Light extends Item {
+    private int Str;
+    
+    public Light (int weight, String name, int str){
+      super(weight, name);
+      this.Str = str;
+    }
+    
+    public int getStr()
+    {
+        return Str;
+    }
     
 }
