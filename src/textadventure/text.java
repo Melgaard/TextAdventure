27de9a6f432667c0af1f6f5744544fee15b0c9f4
SamiMaj <sami.majdoub@gmail.com>
2014-11-25 13:11:24 +0100

    Update text.java
    Start

--- src/textadventure/text.java
+++ src/textadventure/text.java
@@ -5,17 +5,26 @@ package textadventure;
  * @author Blotto
  */
 class text {
+    private static String introText = " had spent many years as squire for the noble king Cappi, before he was knighted and earned the title: Sir Hero\n" +
+"But during the time he was away, his sibling plotted an evil scheme, to kill their father and Sir Hero and inherit the family castle!\n" +
+"Sir Hero as a noble knight cannot stand idly by while his father is murdered! So Sir Hero traveled to his fathers castle to save him.\n" +
+"But his sibling had already arrived and taken their father captive. Sir Hero must enter the castle to free his father!";
     
-    private static String introText;
-    
-    private static String room1IntroText;
+    private static String room1IntroText = "You have entered the castle and now find yourself to be in the entré.\n" +
+"\n" +
+"On your left hand you see some stairs, leading down to the cellar. Right now you see nothing there but darkness.\n" +
+"\n" +
+"In front of you, there is a door. \n" +
+"You remember it to open onto the hallway, but it has been years since you last visited so you are not quite sure.\n" +
+"\n" +
+"On your right, you see the door to the kitchen. Next to the door you see a big painting.";
     private static String room2IntroText;
     private static String room3IntroText;
     private static String room4IntroText;
     private static String room5IntroText;
     private static String room6IntroText;
     private static String room7IntroText;
-
+    
     public static String inputAction(int roomID, String userInput, World gameWorld) {
         
         String[] command = userInput.split(" ");
@@ -89,8 +98,10 @@ class text {
     
     //Getters og Setters
     
-    public static String getIntroText(){
-        return introText;
+    public static String getIntroText(String nameOfHero){
+        return nameOfHero + introText + "\n" + room1IntroText;
     }
+    
+    
     
 }
