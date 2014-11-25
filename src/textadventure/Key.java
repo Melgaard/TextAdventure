a2cf1ccfbc57ad9de6938f12377240cc75661860
ElinorMohr <ElinorMohr@users.noreply.github.com>
2014-11-25 12:09:14 +0100

    Update Key.java
    med nedarv

--- src/textadventure/Key.java
+++ src/textadventure/Key.java
@@ -1,9 +1,17 @@
 package textadventure;
 
-/**
- *
- * @author Blotto
- */
-public class Key {
+
+public class Key extends Item {
+    private int Str;
+    
+    public Key (int weight, String name, int str){
+      super(weight, name);
+      this.Str = str;
+    }
+    
+    public int getStr()
+    {
+        return Str;
+    }
     
 }
