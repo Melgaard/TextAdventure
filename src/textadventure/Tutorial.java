34af3907a82fd081d8a78c4f377168ad92531863
Melgaard <mmjworkgg@gmail.com>
2014-12-01 11:17:14 +0100

    gjorde e til E

--- src/textadventure/Tutorial.java
+++ src/textadventure/Tutorial.java
@@ -16,7 +16,7 @@ public class Tutorial extends JOptionPane {
                 + "\nThis game is a text based adventure game and it is played by writing commands in the input field"
                 + "\nThe commands available in The Adventures of Sir Hero are: "
                 + "\nMove + Direction, this moves you to the next room in the direction if there is a door, the possible directions are: North, South, East, West."
-                + "\nFight enemy, this makes you fight the enemy in the room, fighting compares your strenght and the monsters strenght and the highest wins."
+                + "\nFight Enemy, this makes you fight the enemy in the room, fighting compares your strenght and the monsters strenght and the highest wins."
                 + "\nUse/Search/Take + object, is the way to interact with objects in the game."
                 + "\nAll commands are written with letters and spaces only do NOT write the \"+\".",
                 "Tutorial",
